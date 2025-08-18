ff37dd045983ab16cf702a47a01a35ebb96aea64
Shivaranjan17 <shiv.manali1@gmail.com>
2025-08-18 10:25:58 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,5 @@
+# constants.py
+# Contains major and minor color definitions
+
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
